51f790a26e42d0cd7db19e56b1a2e2e79109dc08
fgoncalvesguilherme <fgoncalves.guilherme@gmail.com>
2023-11-03 00:27:20 -0300

    update

--- data_reading.py
+++ data_reading.py
@@ -2,22 +2,38 @@ import csv
 
 CROPS_LIST = []
 CITIES_LIST = []
+COORDINATES_LIST = []
+TIME_LIST = []
 
 # reading crops data
-with open('data.csv', 'r') as file1:
+with open('data\\data.csv', 'r') as file1:
     reader1 = csv.DictReader(file1)
     
     for line in reader1:
         crop = line['Crops']
         CROPS_LIST.append(crop)
 
-CROPS_LIST = '\n'.join(CROPS_LIST)
-
-with open('cities.csv', 'r') as file2:
+# reading cities data
+with open('data\\cities.csv', 'r') as file2:
     reader2 = csv.DictReader(file2)
     
     for line in reader2:
         city = line['City']
         CITIES_LIST.append(city)
 
-CITIES_LIST = '\n'.join(CITIES_LIST)
+# reading cities coordinates
+with open('data\\cities.csv', 'r') as file3:
+    reader3 = csv.DictReader(file3)
+    
+    for line in reader3:
+        latitude = line['Latitude']
+        longitude = line['Longitude']
+        COORDINATES_LIST.append((latitude, longitude))
+
+# reading time
+with open('data\\data.csv', 'r') as file4:
+    reader4 = csv.DictReader(file4)
+    
+    for line in reader4:
+        time = line['Time']
+        TIME_LIST.append(time)
